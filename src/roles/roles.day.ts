740f6641706b6ab390a6ce58d31beac58ae38c09
ElverTunubala <elvertunubala@gmail.com>
2025-01-31 07:49:05 -0600

    𝗳𝗲𝗮𝘁: 𝗮𝗱𝗱 𝗹𝗼𝗴𝗶𝗻 𝗳𝘂𝗻𝗰𝘁𝗶𝗼𝗻𝗮𝗹𝗶𝘁𝘆

---
+++ src/roles/roles.day.ts
@@ -0,0 +1,10 @@
+export enum DayWeek {
+    LUNES = 'Lunes',
+    MARTES= 'Martes',
+    MIERCOLES='Miercoles',
+    JUEVES='Jueves',
+    VIERNES='Viernes',
+    SABADO='Sabado',
+    DOMINGO='Domingo',
+  }
+  
